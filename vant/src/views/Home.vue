7bdb5cc2f88504b95c3d81aa3cb5c315b360a329
niyinlong <niyinlong@126.com>
2019-03-28 15:00:47 +0800

    网校首页

--- vant/src/views/Home.vue
+++ vant/src/views/Home.vue
@@ -1,16 +1,39 @@
 <template>
 	<div class="home">
-		<ul>
-			<li>
-				<router-link to="/button">button</router-link>
-			</li>
-		</ul>
+		<van-search placeholder="请输入搜索关键词" shape="round" v-model="search" @search="onSearch" @cancel="onCancel">
+		</van-search>
+
+		<van-swipe :autoplay="3000">
+			<van-swipe-item v-for="(image, index) in images" :key="index">
+				<img :src="image" />
+			</van-swipe-item>
+		</van-swipe>
+
 	</div>
 </template>
 
 <script>
 	export default {
-		name: 'home',
+		data() {
+			return {
+				search: '',
+				images: ['https://img.yzcdn.cn/2.jpg',
+					'https://img.yzcdn.cn/2.jpg'
+				]
+			};
+		},
+		methods: {
+			onSearch() {
+				console.log(this.search)
+			},
+			onCancel() {}
+		},
 		components: {}
 	}
 </script>
+
+<style>
+	.van-swipe img {
+		width: 100%;
+	}
+</style>
