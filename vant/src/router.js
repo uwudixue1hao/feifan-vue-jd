173f41f2cc5959edbf0d5c48b0eca8fd0cdd704f
niyinlong <niyinlong@126.com>
2019-03-28 16:56:33 +0800

    商品详情页

--- vant/src/router.js
+++ vant/src/router.js
@@ -3,36 +3,38 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Button from './views/Button.vue'
 import Course from './views/Course.vue'
+import Goods from './views/Goods.vue'
 import User from './views/User.vue'
 import Learn from './views/Learn.vue'
-
-
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },{
-      path: '/course',
-      name: 'course',
-      component: Course
-    },{
-      path: '/learn',
-      name: 'learn',
-      component: Learn
-    },{
-      path: '/user',
-      name: 'user',
-      component: User
-    },{
-      path: '/button',
-      name: 'button',
-      component: Button
-    }]
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes: [{
+		path: '/',
+		name: 'home',
+		component: Home
+	}, {
+		path: '/course',
+		name: 'course',
+		component: Course
+	}, {
+		path: '/goods',
+		name: 'goods',
+		component: Goods
+	}, {
+		path: '/learn',
+		name: 'learn',
+		component: Learn
+	}, {
+		path: '/user',
+		name: 'user',
+		component: User
+	}, {
+		path: '/button',
+		name: 'button',
+		component: Button
+	}]
 })
---
+++ vant/src/views/Goods.vue
@@ -0,0 +1,66 @@
+<template>
+	<div class="goods">
+
+		<van-swipe class="goods-swipe" :autoplay="3000">
+			<van-swipe-item v-for="thumb in images" :key="thumb">
+				<img :src="thumb">
+			</van-swipe-item>
+		</van-swipe>
+
+		<van-cell-group>
+			<van-cell>
+				<div class="goods-title">{{title }}</div>
+				<div class="goods-price">{{ price }}</div>
+			</van-cell>
+			<van-cell class="goods-express">
+				<van-col span="10">运费：{{ express }}</van-col>
+				<van-col span="14">剩余：{{ remain }}</van-col>
+			</van-cell>
+		</van-cell-group>
+
+		<van-cell-group class="goods-cell-group">
+			<van-cell value="进入店铺" icon="shop-o" is-link @click="sorry">
+				<template slot="title">
+					<span class="van-cell-text">有赞的店</span>
+					<van-tag class="goods-tag" type="danger">官方</van-tag>
+				</template>
+			</van-cell>
+			<van-cell title="线下门店" icon="location-o" is-link @click="sorry" />
+		</van-cell-group>
+
+		<van-cell-group class="goods-cell-group">
+			<van-cell title="查看商品详情" is-link @click="sorry" />
+		</van-cell-group>
+
+		<van-goods-action>
+			<van-goods-action-mini-btn icon="chat-o" text="客服" />
+			<van-goods-action-mini-btn icon="shop-o" text="店铺" />
+			<van-goods-action-mini-btn info="5" icon="cart-o" text="购物车" />
+			<van-goods-action-big-btn text="加入购物车" />
+			<van-goods-action-big-btn primary text="立即报名" />
+		</van-goods-action>
+	</div>
+
+</template>
+
+<script>
+	export default {
+
+		data() {
+			return {
+				title:'商品标题',
+				price:12.65,
+				express:12,
+				remain:1000,
+				images: ['https://img.yzcdn.cn/public_files/2017/10/24/e5a5a02309a41f9f5def56684808d9ae.jpeg',
+					'https://img.yzcdn.cn/public_files/2017/10/24/1791ba14088f9c2be8c610d0a6cc0f93.jpeg'
+				]
+			};
+		}
+
+	}
+</script>
+
+<style>
+
+</style>
