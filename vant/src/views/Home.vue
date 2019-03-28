211e453158f36b7e60e88ef77dffc2d8b120fa9c
niyinlong <niyinlong@126.com>
2019-03-28 14:38:37 +0800

    创建3个空白项目

---
+++ vant/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="home">
+		<ul>
+			<li>
+				<router-link to="/button">button</router-link>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'home',
+		components: {}
+	}
+</script>
